4e2b97e20f0414e94249e88c45bf9198e60c2600
shyrvik <shyrvik@gmail.com>
2023-05-06 02:48:56 +0300

    update key_status

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
 use std::{io, thread};
 use std::time::Duration;
 use clearscreen;
+use device_query::{DeviceQuery, DeviceState, Keycode};
 
 const WIDTH: usize = 7;
-const HEIGHT: usize = 7;
+const HEIGHT: usize = 10;
 const FILL: i8 = 0;
+
 const TOY: i8 = 1;
-const TIME_UPD: u64 = 3300; //time screen update and level of hard game
-const TIME_KEY: u64 = 300;
-const LEFT: char = 'a';
-const RIGHT: char = 'd';
-const UP: char = 'w';
-const DOWN: char = 's';
+const START_PST: usize = 3;
+
+const TIME_UPD: u64 = 800; //time screen update and level of hard game
+const TIME_KEY: u64 = 500;
+
+const LEFT:Keycode = Keycode::A;
+const RIGHT: Keycode = Keycode::D;
+const UP: Keycode = Keycode::W;
+const DOWN: Keycode = Keycode::S;
+//const LEFT_B: u8 = 01000001;
 
 fn main () {
 
     let mut pole: Vec<Vec<i8>> = vec![vec![FILL;WIDTH];HEIGHT];
     let mut pole_variable: Vec<Vec<i8>>; 
-
+/*
+    let handle = thread::spawn(move || {
+        loop {
+        let m: u8 = key_status();
+        println!("{}", m);
+        }
+    });
+*/
     loop {
-        pole[0][3] = TOY;  //function for generate TOY (update next...)
-
+        pole[0][START_PST] = TOY;
+        
         for string_cnt in 0..HEIGHT { //print vector on terminal
-                //pole_variable = update_screen(&mut pole); 
-
+                pole_variable = update_screen(&mut pole); 
+                
                 for x in &pole_variable {
                     for y in x {
                     print!("{} ", y);
@@ -31,19 +44,22 @@ fn main () {
                 println!();
                 }
                 
-                pole = pole_variable; //update pole on last iteration
+                pole = pole_variable;    //update pole on last iteration
                 thread::sleep(Duration::from_millis(TIME_UPD)); // sleep to update pole
                 clearscreen::clear().expect("Tetris failed");
+                
         }
     }
+    //handle.join().unwrap(); 
+}
+
+fn pole() {
+
 }
 
 fn update_screen(vc: &mut Vec<Vec<i8>>) -> Vec<Vec<i8>> {
-
-    let mut status_key: String = String::new();
    
     'count: for h in 0..(HEIGHT - 1) { //update_pixel height
-            let m: i32 = key_status();
             
                 for w in 0..(WIDTH - 1) { //update_pixel width
                     match *&vc[h][w] {
@@ -59,25 +75,28 @@ fn update_screen(vc: &mut Vec<Vec<i8>>) -> Vec<Vec<i8>> {
                         }
                     }
                 }
-
     return vc.to_vec()
 }
 
-fn key_status() -> i32 {
 
-    let mut status_key: String = String::new();
-    io::stdin().read_line(&mut status_key); //{
 
-    let key: Vec<char> = status_key.chars().collect();
-    let mut position: i32 = 0;
+fn key_status() -> u8 {
 
-    match key[0] {
-        'a' => position += 1,
-        'd' => position += 2,
-        'w' => position += 3,
-        's' => position += 4,
-        _ => println!{"Err read key"},
-    };
+        let device_state = DeviceState::new(); //read key status
 
+        thread::sleep(Duration::from_millis(300));
+
+        let keys: Vec<Keycode> = device_state.get_keys(); //get keys
+        let mut position: u8 = 0;
+        
+        if device_state.get_keys().len() > 0 { // get position toy
+            match keys[0] {
+                LEFT => position += 1,
+                RIGHT => position += 2,
+                UP => position += 3,
+                DOWN => position += 4,
+                _ => {},
+            }
+        }
     position
 }
