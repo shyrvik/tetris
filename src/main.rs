413a25c1b899b0553337f6f97d32caa3f409dfd8
shyrvik <shyrvik@gmail.com>
2023-05-30 02:08:07 +0300

    tetris v1.0

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use clearscreen;
 use device_query::{DeviceQuery, DeviceState, Keycode};
 use std::sync::{Arc, Mutex};
 use rand::Rng;
-//use std::sync::mpsc;
+use std::sync::mpsc;
 
-const WIDTH: usize = 10;
-const HEIGHT: usize = 16;
+const WIDTH: usize = 14;
+const HEIGHT: usize = 24;
 const FILL: u8 = 0;
 
 const TOY: u8 = 2;
@@ -16,7 +16,7 @@ const STATIC_TOY: u8 = 1;
 const START_PST: u8 = 3;
 
 const TIME_UPD: u64 = 700;
-const TIME_KEY: u64 = 60;
+const TIME_KEY: u64 = 80;
 
 const LEFT:Keycode = Keycode::Left;
 const RIGHT: Keycode = Keycode::Right;
@@ -36,24 +36,55 @@ fn main() {
     global_flag_change(true);
     loop_flag_change(true);
     toy_flag_change(true);
-    //let (tx, rx) = mpsc::channel();                                             //create channel thread_1 tread_2
     
+    let mutex_toy = Arc::new(Mutex::new(vec![vec![FILL;2];3]));
     let mutex_pole = Arc::new(Mutex::new(vec![vec![FILL;WIDTH];HEIGHT]));
 
-    let toy1 = Arc::new(Mutex::new(rand_toy()));
-   
+    let mutex_toy_2 = Arc::clone(&mutex_toy);
+    let mutex_arc_2 = Arc::clone(&mutex_pole);
+    let handle_two = thread::spawn(move || {
+        let mut count = 0;
+        let mut toy_now: Vec<Vec<u8>> = vec![vec![FILL;2];3]; 
 
-    let toy_arc = Arc::clone(&toy1);
+        loop {
+            thread::sleep(Duration::from_millis(global_time_status()));
+
+            let mut toy_now = mutex_toy_2.lock().unwrap(); 
+            let mut pole_p = mutex_arc_2.lock().unwrap();
+            let mut received_key: i8 = 0;
+             
+            if  toy_flag_status() {
+                count = 0;
+                pole_p[HEIGHT-1][0] = 2;
+                pole_p[HEIGHT-1][1] = START_PST; 
+                rand_toy(&mut toy_now);
+                toy_flag_change(false);
+            }
+            
+            if loop_flag_status() { 
+                clearscreen::clear().expect("Tetris failed");
+                pole_print(update_screen(&mut pole_p, &mut received_key, &mut toy_now));
+                
+            }
+            if count != HEIGHT-1 {
+                count += 1;
+            } else {count = 0}
+            //println!("{}", &count);
+        }
+    });
+
+    let mutex_toy = Arc::clone(&mutex_toy);
     let mutex_arc = Arc::clone(&mutex_pole);
     let handle_one = thread::spawn(move || {
+    let mut toy_k: Vec<Vec<u8>> = vec![vec![FILL;2];3];
+
         loop {
             thread::sleep(Duration::from_millis(TIME_KEY));
-            let mut toy_k = toy_arc.lock().unwrap();
+
+            let mut toy_k = mutex_toy.lock().unwrap();
             let mut pole_k = mutex_arc.lock().unwrap();
-            let mut num_key: i8 = key_status();     //читаем состояние клавиши
-                                                           
-            if global_flag_status() && toy_flag_status() != true {     // разрешение на віполнение в момент обновления поля
-                loop_flag_change(false);
+            let mut num_key: i8 = key_status();   //читаем состояние клавиши
+            if toy_flag_status() != true {
                 match num_key {
                     LE | RI | ROT  => {
                         clearscreen::clear().expect("Tetris failed");
@@ -74,54 +105,37 @@ fn main() {
         }
     });
 
-    let toy_arc_2 = Arc::clone(&toy1);
-    let mutex_arc_2 = Arc::clone(&mutex_pole);
-    let handle_two = thread::spawn(move || {
-        let mut count = 0;
-
-        
-        loop {
-            thread::sleep(Duration::from_millis(global_time_status()));
-            let mut toy_p = toy_arc_2.lock().unwrap();
-            let mut pole_p = mutex_arc_2.lock().unwrap();
-            let mut received_key: i8 = 0;
-
-            if  toy_flag_status() {
-                    count = 0;
-                    pole_p[HEIGHT-1][0] = 2;
-                    pole_p[HEIGHT-1][1] = START_PST;
-                    add_toy_in_pole(&mut pole_p, &mut toy_p, 0, START_PST as usize);
-                toy_flag_change(false);
-            }
-            if loop_flag_status() { 
-                clearscreen::clear().expect("Tetris failed");
-                pole_print(update_screen(&mut pole_p, &mut received_key, &mut toy_p));
-            }
-            if count != HEIGHT-1 {
-                count += 1;
-            } else {count = 0}
-            println!("{}", &count);
-        }
-    });
-
     handle_one.join().unwrap(); 
     handle_two.join().unwrap(); 
 }
 
 fn pole_print(pole_step:  Vec<Vec<u8>> ) {
-        for element_cnt in &pole_step[0..pole_step.len()-1] {
-            for y in element_cnt {
-                let fill: String = String::from("..");
-                let toy: String = String::from("[]");
-                match *y {
-                    FILL => print!("{}", fill),
-                    STATIC_TOY => print!("{}", toy),
-                    TOY => print!("{}", toy),
-                    _=> {},
-                }
+    for n in 0..WIDTH-1 {
+        print!("##");
+    }
+    println!();
+    for string_cnt in 4..HEIGHT-1 {
+        for y in 0..WIDTH {
+            let fill: String = String::from("''");
+            let toy: String = String::from("[]");
+            match pole_step[string_cnt][y] {
+                FILL => {
+                    if y == 0 || y == WIDTH-1 {
+                        print!("#");
+                    } else {
+                    print!("{}", fill)
+                    }
+                },
+                STATIC_TOY => print!("{}", toy),
+                TOY => print!("{}", toy),
+                _=> {},
             }
-        println!();
         }
+    println!();
+    }
+    for n in 0..WIDTH-1 {
+        print!("##");
+    }
     for _n in 1..5  {
          println!();
     }
@@ -249,35 +263,44 @@ fn rotate_toy(rot_toy12: &mut Vec<Vec<u8>>) {
     let width: usize = rot_toy[0].len() as usize;
 
     if width < height {
-        rot_toy12.remove(height-1);
-        for w in 0..width {
-            rot_toy12[w].push(0);
+        for h in width..height {
+            rot_toy12.remove(h);
         }
-    } else {
-        rot_toy12.push(vec![0; height]);
-        for h in 0..height {
-            rot_toy12[h].remove(width-1);
+        for h in 0..width {
+            for w in height..width {
+                rot_toy12[h].push(0);
+            }
+        }
+    } 
+    if width > height {
+        for h in height..width {
+            rot_toy12.push(vec![0; height]);
+        }
+        for h in 0..width {
+            for w in height..width {
+                rot_toy12[h].remove(w);
+            }
         }
     }
+
+    let height_rot_toy12: usize = rot_toy12.len() as usize;
+    let width_rot_toy12: usize = rot_toy12[0].len() as usize;
 
     for h in 0..height {
         for w in 0..width {
-
-            rot_toy12[width-1-w][height-1-h] = rot_toy[h][w];
+            rot_toy12[w][height_rot_toy12 - h - 1] = rot_toy[h][w];
         } 
     } 
-
+    
+    let clone_rot_toy12 = rot_toy12.clone();
     if width < height {
-        let arr_toy = rot_toy12.clone(); 
-        let height_new = rot_toy12.len();
-        let width_new = rot_toy12[0].len();
-
-        for h in 0..height_new {
-            for w in 0..width_new {
-                rot_toy12[height_new- 1 - h][width_new- 1 - w] = arr_toy[h][w];
-            } 
-        } 
+    for h in 0..height_rot_toy12 {
+        for w in 0..width_rot_toy12 {
+            rot_toy12[h][width_rot_toy12 - w - 1] = clone_rot_toy12[h][w];
+        }
     }
+    }
+    
 }
 
 fn clear_string(vc_upd: &mut Vec<Vec<u8>>) {
@@ -305,19 +328,60 @@ fn clear_string(vc_upd: &mut Vec<Vec<u8>>) {
     }
 }
 
-fn rand_toy() -> Vec<Vec<u8>> {
-    let mut rng = rand::thread_rng();                                  //
+fn rand_toy(toy: &mut Vec<Vec<u8>>) {
+    let mut rng = rand::thread_rng();                                  //запускаем поток перебора
 
-    let mut toy11: Vec<Vec<u8>> = vec![vec![FILL;2];3];                          // 222
+    let mut toy11: Vec<Vec<u8>> = vec![vec![FILL;3];3];                          // 222
     toy11[0][1] = TOY; toy11[1][1] = TOY; toy11[2][1] = TOY; toy11[1][0] = TOY;  //  2
 
-    let mut toy12: Vec<Vec<u8>> = vec![vec![FILL;2];3];                          //  22
+    let mut toy12: Vec<Vec<u8>> = vec![vec![FILL;3];3];                          //  22
     toy12[0][0] = TOY; toy12[1][0] = TOY; toy12[1][1] = TOY; toy12[2][1] = TOY;  //   22
 
-    let toy_arr = [toy11, toy12];
+    let mut toy13: Vec<Vec<u8>> = vec![vec![FILL;3];3];                          //  2
+    toy13[0][0] = TOY; toy13[1][0] = TOY; toy13[2][0] = TOY; toy13[2][1] = TOY;  //  222
+
+    let mut toy14: Vec<Vec<u8>> = vec![vec![FILL;4];4];                          //  2
+    toy14[0][0] = TOY; toy14[0][1] = TOY; toy14[0][2] = TOY; toy14[0][3] = TOY;  //  222
+
+    let toy_arr = [toy11, toy14, toy13, toy12];
     let random_index = rng.gen_range(0..toy_arr.len());
     let toy_value: &Vec<Vec<u8>> = &toy_arr[random_index];
-    toy_value.to_vec()
+
+    let height_toy: usize = toy.len() as usize;
+    let width_toy: usize = toy[0].len() as usize;
+    let height: usize = toy_value.len() as usize;
+    let width: usize = toy_value[0].len() as usize;
+
+    if height_toy < height {
+        for h in height_toy..height {
+            toy.push(vec![FILL; width]);
+        }
+    }
+    if height_toy > height {
+        for h in height..height_toy {
+            toy.remove(h);
+        }
+    }
+    if width_toy < width {
+        for h in 0..height {
+            for w in width_toy..width {
+                toy[h].push(FILL);
+            }
+        }
+    }
+    if width_toy > width {
+        for h in 0..height {
+            for w in width..width_toy {
+                toy[h].remove(w);
+            }
+        }
+    }
+
+    for h in 0..height {
+        for w in 0..width {
+            toy[h][w] = toy_value[h][w];
+        }
+    }
 }
 
 fn key_status() -> i8 {
